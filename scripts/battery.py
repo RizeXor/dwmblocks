9c7584217cba6a317760ce849777bb86f313fb87
RizeXor <rize@serverstack.xyz>
2020-11-19 21:48:59 +0530

    support for desktops, with no battery

--- scripts/battery.py
+++ scripts/battery.py
@@ -1,11 +1,13 @@
-status_file = open("/sys/class/power_supply/BAT0/status", "r")
-percent_file = open("/sys/class/power_supply/BAT0/capacity", "r")
+try:
+    status_file = open("/sys/class/power_supply/BAT0/status", "r")
+    percent_file = open("/sys/class/power_supply/BAT0/capacity", "r")
 
-status = status_file.readline().rstrip('\n')
-percent = percent_file.readline().rstrip('\n')
+    status = status_file.readline().rstrip('\n')
+    percent = percent_file.readline().rstrip('\n')
 
-print(f"{status} {percent}%", end="\n", sep="")
+    print(f"{status} {percent}%", end="\n", sep="")
 
-status_file.close()
-percent_file.close()
-
+    status_file.close()
+    percent_file.close()
+except:
+    print("Desktop")
