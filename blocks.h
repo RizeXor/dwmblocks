f4600a0732281f2d9adc0d95d5ffbbff9de0eff2
RizeXor <decodestudios.help@gmail.com>
2020-11-25 13:05:51 +0530

    added cpu, formatted date

--- blocks.h
+++ blocks.h
@@ -1,12 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
-	// {"", "cat /sys/class/power_supply/BAT0/capacity",					5,		0},
-	{"", "python3 /home/rize/git/dwmblocks/scripts/battery.py",					5,		0},
-	{"", "pamixer --get-volume",					3,		0},
+	{"",        "date '+%b %d %a %I:%M %p'",					                    10,		0},
+	{"Mem:",    "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	        30,		0},
+	{"Cpu:",    "/home/rize/git/dwmblocks/scripts/cpu.sh",	                        5,		0},
+	{"",        "python3 /home/rize/git/dwmblocks/scripts/battery.py",		        5,		0},
+	{"",        "pamixer --get-volume",					                            3,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
